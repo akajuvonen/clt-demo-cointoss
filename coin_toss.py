b401086c7d3f4b517d23001932d6bfa56b9986d8
Antti Juvonen <akajuvonen@users.noreply.github.com>
2018-02-16 17:14:32 +0200

    Added command line argument parser

--- coin_toss.py
+++ coin_toss.py
@@ -1,13 +1,23 @@
 import random
 from collections import Counter
 import matplotlib.pyplot as plt
+import argparse
 
 def coin_toss(flips):
     return [x for x in [random.randint(0,1) for x in range(flips)]]    
 
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--flips', type=int, help='number of flips', default='100')
+    parser.add_argument('-i', '--iterations', type=int, help='number of iterations', default='1000')
+    args = vars(parser.parse_args())
+    flips = args['flips']
+    iterations = args['iterations']
+    return flips, iterations
+
 def main():
-    flips = 100
-    iterations = 1000
+    flips, iterations = parse_arguments()
+    print('Calculating distribution with {} flips and {} iterations'.format(flips, iterations))
     dist = Counter([x for x in [sum([x==1 for x in coin_toss(flips)]) for x in range(iterations)]])
     plt.bar(dist.keys(), dist.values())
     plt.show()
